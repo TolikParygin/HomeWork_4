081e594d3f55a8358e5467e9ff3f31d6547af2ab
Tolik <anatolijparygin988@gmail.com>
2022-12-05 22:49:39 +0300

    задаем и выводим массив на экран

---
+++ Task_3/Program.cs
@@ -0,0 +1,26 @@
+﻿//  Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+Console.Clear();
+void FillArray(int[] array)
+{
+    int length = array.Length;
+
+    for (int index = 0; index < length; index++)
+    {
+        array[index] = new Random().Next(1, 100);// так как в условии задачи программа задает числа, поэтому я взял рандом от 1 до 99.
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    for (int position = 0; position < count; position++)
+    {
+        Console.Write(col[position] + "  ");
+    }
+}
+
+int[] array = new int[8];
+FillArray(array);
+PrintArray(array);
