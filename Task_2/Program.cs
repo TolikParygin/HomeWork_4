ab9e472dd5460072716208e47f673fed09e8d244
Tolik <anatolijparygin988@gmail.com>
2022-12-05 21:52:29 +0300

    сумма цифр числа

---
+++ Task_2/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+Console.Clear();
+int FindSum(int number)
+{
+    int sum = 0;
+    while (number != 0)
+    {
+        sum = sum + number % 10;
+        number = number / 10;
+    }
+    return sum;
+}
+
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+Console.WriteLine($"{number} -> {FindSum(number)}");
