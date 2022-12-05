bd79ec1edf2c2e60649211dfbb5a4530658166d9
Tolik <anatolijparygin988@gmail.com>
2022-12-05 21:29:42 +0300

    число А в степени числа В

---
+++ Task_1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+Console.Clear();
+int FineDegree(int firstNumber, int secondNumber)
+{
+    int result = 1;
+    for (int count = 0; count < secondNumber; count++)
+    {
+        result = result * firstNumber;
+    }
+    return result;
+}
+
+
+Console.Write("Введите первое число: ");
+int firstNumber = int.Parse(Console.ReadLine());// Вводим первое число
+Console.Write("Введите первое число: ");
+int secondNumber = int.Parse(Console.ReadLine());// Вводим второе число
+Console.WriteLine($"{firstNumber} , {secondNumber} -> {FineDegree(firstNumber, secondNumber)}");
